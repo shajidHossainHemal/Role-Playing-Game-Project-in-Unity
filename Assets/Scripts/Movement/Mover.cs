2b4a04ae7f74cc50b2c84de833a7414103750c09
Shajid Hossain <sajidhossainhemal@gmail.com>
2020-12-06 22:09:42 +0600

    Basic Combat Functionality

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement 
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        private NavMeshAgent navMeshAgent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination) {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination) {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel() {
+            navMeshAgent.isStopped = true;
+        }
+        
+        private void UpdateAnimator() {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
